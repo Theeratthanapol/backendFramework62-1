bda6505738189e0ea91520bdd2826650971ee4c3
Theeratthanapol <theeratthanapol@gmail.com>
2019-05-02 15:28:33 +0700

    Test deploy.

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const Express = require("express");
-const bodyParser = require("body-parser");
+const Express = require('express');
+const bodyParser = require('body-parser');
 
 const app = new Express();
 
 app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
     extended: true
-  })
-);
+}));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "ContentType, x-access-token");
-  next();
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
+    next();
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, () => {
-  console.log(`Running on http://localhost:${port}`);
+    console.log(`Running on http://localhost:${port}`);
 });
 
-const login = require("./routes/login"); //
-app.use("/login", login);
+const login = require('./routes/login'); // 
+app.use('/login', login);
 
-const user = require("./routes/user"); //
-app.use("/user", user);
+const user = require('./routes/user'); // 
+app.use('/user', user);
