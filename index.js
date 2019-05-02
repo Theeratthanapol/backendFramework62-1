8870df5be668858adacfecf1baeab0fb8cfb99fd
“theeratthnapol” <theeratthanapol@gmail.com>
2019-05-02 12:45:25 +0700

    Add_generate_jwt_Aladin53

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "ContentType, x-access-token");
+  res.header("Access-Control-Allow-Headers", "ContentType, x-access-token"); 
   next();
 });
 
